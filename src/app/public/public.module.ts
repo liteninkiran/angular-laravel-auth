9402c11bdadd190202559e4530ea224f539f92c2
Kiran Anand <liteninkiran@hotmail.com>
2021-09-07 20:37:23 +0100

    Add login component

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     declarations: [
         PublicComponent,
         HomeComponent,
+        LoginComponent,
     ],
     imports: [
         CommonModule,
