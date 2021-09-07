d04d0ca91bd29d5058a49ae25058b40a93fcfb18
Kiran Anand <liteninkiran@hotmail.com>
2021-09-08 00:22:45 +0100

    Create login form

--- frontend/src/app/public/login/login.component.ts
+++ frontend/src/app/public/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-login',
@@ -8,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 
 export class LoginComponent implements OnInit {
 
-    constructor() { }
+    public form: FormGroup;
+
+    constructor(private fb: FormBuilder, private http: HttpClient) { }
 
     ngOnInit(): void {
+        this.form = this.fb.group({
+            email: '',
+            password: '',
+        });
+    }
+
+    public submit() {
+        const formData = this.form.getRawValue();
+
+        const data = {
+            username: formData.email,
+            password: formData.password,
+            grant_type: 'password',
+            client_id: 2,
+            client_secret: 'ZLY5UOVF8pUN7Wn49XS6bFA3L4y2AmmeSLmWI1HX',
+            scope: '*',
+        };
+
+        const url = 'http://localhost:8000/oauth/token';
+
+        this.http.post(url, data).subscribe(
+            result => {
+                console.log('Success');
+                console.log(result);
+            }, error => {
+                console.log('Error');
+                console.log(error);
+            }
+        );
     }
 
 }
--- frontend/src/app/public/public.module.ts
+++ frontend/src/app/public/public.module.ts
@@ -4,6 +4,8 @@ import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -14,6 +16,8 @@ import { RouterModule } from '@angular/router';
     imports: [
         CommonModule,
         RouterModule,
+        ReactiveFormsModule,
+        HttpClientModule,
     ]
 })
 
