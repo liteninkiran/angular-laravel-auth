35abc22ea855140d006c87d8c8608760a9bd4bcf
Kiran Anand <liteninkiran@hotmail.com>
2021-09-07 20:28:49 +0100

    Add public component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'public', component: PublicComponent }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
