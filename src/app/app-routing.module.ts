bb9123a2e1e71e6f1a7feca98cecbd34d166782b
Kiran Anand <liteninkiran@hotmail.com>
2021-09-07 20:32:30 +0100

    Add secure component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
 const routes: Routes = [
-    { path: 'public', component: PublicComponent }
+    { path: 'public', component: PublicComponent },
+    { path: 'secure', component: SecureComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicComponent } from './public/public.component';
+import { SecureComponent } from './secure/secure.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         PublicComponent,
+        SecureComponent,
     ],
     imports: [
         BrowserModule,
