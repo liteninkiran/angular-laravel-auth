c84ea07f8c33217ccdaaf36e4f1762fa1d6ec355
Kiran Anand <liteninkiran@hotmail.com>
2021-09-07 20:50:54 +0100

    Routing for login and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PublicModule } from './public/public.module';
 import { SecureComponent } from './secure/secure.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { PublicModule } from './public/public.module';
 
 @NgModule({
     declarations: [
@@ -15,6 +17,7 @@ import { SecureComponent } from './secure/secure.component';
         BrowserModule,
         AppRoutingModule,
         PublicModule,
+        RouterModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PublicComponent } from './public.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [
@@ -12,6 +13,7 @@ import { LoginComponent } from './login/login.component';
     ],
     imports: [
         CommonModule,
+        RouterModule,
     ]
 })
 
